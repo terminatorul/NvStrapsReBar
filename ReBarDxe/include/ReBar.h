7635789772616af7fa98256e9dac3d87d12f7249
Timothy Madden <terminatorul@gmail.com>
2024-02-26 16:26:34 +0200

    Add call to s3SaveState.Write() method.

--- ReBarDxe/include/ReBar.h
+++ ReBarDxe/include/ReBar.h
@@ -2,10 +2,12 @@
 #define REBAR_UEFI_REBAR_H
 
 #include <Uefi.h>
+#include <Protocol/S3SaveState.h>
 
 #include "NvStrapsConfig.h"
 
 extern EFI_HANDLE reBarImageHandle;
 extern NvStrapsConfig *config;
+extern EFI_S3_SAVE_STATE_PROTOCOL *S3SaveState;
 
 #endif          // !defined(REBAR_UEFI_REBAR_H)
